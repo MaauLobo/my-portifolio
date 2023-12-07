6b29e2e9f8324da67951abfada4f0180b281ea34
unknown <mauriciolobo509@gmail.com>
2023-12-07 01:16:47 -0300

    component work feito para tela about

---
+++ src/components/work.component.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="work">
+        <h1>Experiências Profissionais</h1>
+        <div class="work1">
+            <h2>Suporte de Software</h2> 
+            <div class="company-info">
+            <p>
+                <font-awesome-icon :icon="['fas', 'building']" />
+                P7GO
+            </p>
+            <p>
+                <font-awesome-icon :icon="['fas', 'location-dot']" />
+                Feira De Santana
+            </p>
+            </div>
+        </div>
+        <span class="separator"></span>
+    </div>
+</template>
+
+<script>
+    import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+    import { library } from '@fortawesome/fontawesome-svg-core';
+    import { fas } from '@fortawesome/free-solid-svg-icons';
+
+    library.add(fas);
+
+    export default {
+        components: {
+            FontAwesomeIcon,
+        },
+    };
+</script>
+
+<style>
+    @import "@/styles/about.css";
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,19 @@
 <template>
 <navcomp/>
 <aboutme/>
+<work/>
 
 </template>
 
 <script>
 import navcomp from '../components/nav.component.vue';
 import aboutme from '../components/aboutme.component.vue';
+import work from '../components/work.component.vue';
 export default {
     components:{
         navcomp,
         aboutme,
+        work,
     }
 }
 </script>
